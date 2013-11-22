e545ff4d7c5939ee01a714c8c3d3931479455930
Ulysses Grundler <grundler@cern.ch>
2013-11-22 12:21:10 +0100

    Expand the dcolMonitor to 500k events

--- src/EUTelConvertCMSPixel.cc
+++ src/EUTelConvertCMSPixel.cc
@@ -493,7 +493,7 @@ void EUTelConvertCMSPixel::bookHistos() {
 
     string dcolMonitorEvtTitle = "DCOL hits over event #, ROC" + to_string( iDetector ) + ";event # / 100;column ID;hits per 100 events";
     tempHistoName = _dcolMonitorEvtHistoName + "_d" + to_string( iDetector );
-    AIDA::IHistogram2D * dcolMonitorEvtHisto = AIDAProcessor::histogramFactory(this)->createHistogram2D( (basePath + tempHistoName).c_str(), 101, 0, 10000, 52, 0, 51);
+    AIDA::IHistogram2D * dcolMonitorEvtHisto = AIDAProcessor::histogramFactory(this)->createHistogram2D( (basePath + tempHistoName).c_str(), 5001, 0, 500000, 52, 0, 51);
     _aidaHistoMap.insert(make_pair(tempHistoName, dcolMonitorEvtHisto));
     dcolMonitorEvtHisto->setTitle(dcolMonitorEvtTitle.c_str());
 				
